849b80cffd5081d1c02321760e775c11889a895b
Charlie <charkrios@gmail.com>
2025-03-25 20:39:04 +0100

    custom vite config -> @ for global src/ import path

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import path from "path";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
+//He importado el metodo path para usar @ en los imports de archivos dentro de "./src"
 export default defineConfig({
   plugins: [react()],
-})
+  server: { port: 5173 },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+});
