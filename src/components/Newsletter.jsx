a3d424fbb037ca8e2d79ea64254307b0d7524131
Charlie <charkrios@gmail.com>
2025-03-25 21:13:03 +0100

    new Newsletter component

---
+++ src/components/Newsletter.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { Container, Row, Col, Form, Button } from "react-bootstrap";
+import mailIcon from "@/assets/newsletter/icon/mail.svg";
+import "@/styles/newsletter.css";
+
+const Newsletter = () => {
+  return (
+    <Container fluid className="newsletter-section">
+      <Container className="py-10">
+        <Row className="justify-content-center text-center">
+          <Col md={8}>
+            <h1 className="newsletter-title mb-4">Do you have any questions?</h1>
+            <h4 className="newsletter-subtitle mb-5">Let us help you!</h4>
+          </Col>
+        </Row>
+        <Row className="justify-content-center">
+          <Col md={8}>
+            <div className="d-flex newsletter-form-container">
+              <div className="newsletter-input-wrapper">
+                <img src={mailIcon} alt="Email icon" className="newsletter-icon" />
+                <Form.Control
+                  type="email"
+                  placeholder="Enter your email"
+                  className="newsletter-input"
+                />
+              </div>
+              <Button className="newsletter-submit-btn">Submit</Button>
+            </div>
+          </Col>
+        </Row>
+      </Container>
+    </Container>
+  );
+};
+
+export default Newsletter;
